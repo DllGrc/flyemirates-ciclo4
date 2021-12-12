7f896fd5393e2b2f58bca2d85f1a3e2b1b881b20
David <dllgrca@gmail.com>
2021-12-11 22:28:49 -0500

    CRUD Vuelos Implementado

--- flyEmirates/src/app/modulos/vuelos/create/create.component.ts
+++ flyEmirates/src/app/modulos/vuelos/create/create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { VueloModelo } from 'src/app/modelos/vuelo.model';
+import { VuelosService } from 'src/app/servicios/vuelos.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-create',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private vueloService: VuelosService, private router: Router) { }
+
+  fgValidacion = this.fb.group({
+    fechaInicio: ['', [Validators.required]],
+    horaInicio: ['', [Validators.required]],
+    fechaFin: ['', [Validators.required]],
+    horaFin: ['', [Validators.required]],
+    asientosVendidos: ['', [Validators.required]],
+    nombrePiloto: ['', [Validators.required]],
+    ruta: ['', [Validators.required]],
+  });
 
   ngOnInit(): void {
   }
+
+  store(){
+    let vuelo = new VueloModelo();
+    vuelo.fechaInicio = this.fgValidacion.controls["fechaInicio"].value;
+    vuelo.horaInicio = this.fgValidacion.controls["horaInicio"].value;
+    vuelo.fechaFin = this.fgValidacion.controls["fechaFin"].value;
+    vuelo.horaFin = this.fgValidacion.controls["horaFin"].value;
+    vuelo.asientosVendidos = this.fgValidacion.controls["asientosVendidos"].value;
+    vuelo.nombrePiloto = this.fgValidacion.controls["nombrePiloto"].value;
+    vuelo.ruta = this.fgValidacion.controls["ruta"].value;
+ 
+    this.vueloService.store(vuelo).subscribe((data: VueloModelo)=> {
+      Swal.fire('Creado correctamente!', '', 'success')
+      this.router.navigate(['/vuelos/get']);
+    },
+    (error: any) => {
+      console.log(error)
+      alert("Error en el envio");
+    })
+  }
+
 
 }
